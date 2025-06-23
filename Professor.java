328fd6b4ebd4e0e3b93b7aa9bb3a8cacc7a59f25
Isa Gerstner <isabellagerstner16@gmail.com>
2025-06-23 18:39:57 -0300

    blehbleh

--- Professor.java
+++ Professor.java
@@ -1,3 +1,17 @@
 public class Professor {
-    
+    private String nome;
+    private String titulacao;
+    private int matricula;
+
+    public Professor(String nome, String titulacao, int matricula) {
+        this.nome = nome;
+        this.titulacao = titulacao;
+        this.matricula = matricula;
+    }
+
+    public void adicionarProfessor(String nome, String titulacao, int matricula) {
+        this.nome = nome;
+        this.titulacao = titulacao;
+        this.matricula = matricula;
+    }
 }
--- Turma.java
+++ Turma.java
@@ -1,3 +1,37 @@
 public class Turma {
-    
+    private String dia;
+    private String horario;
+    private int quantAlunos;
+    private Disciplina disciplina;
+    private Professor professor;
+
+    public Turma(String dia, String horario, int quantAlunos, Disciplina disciplina, Professor professor) {
+        this.dia = dia;
+        this.horario = horario;
+        this.quantAlunos = quantAlunos;
+        this.disciplina = disciplina;
+        this.professor = professor;
+    }
+
+    public int getQuantAlunos() {
+        return quantAlunos;
+    }
+
+    public static double mediaQtdAlunos(Turma[] turmas) {
+        int total = 0;
+        for (Turma t : turmas) {
+            total += t.getQuantAlunos();
+        }
+        return (double) total / turmas.length;
+    }
+
+    public static Turma buscarMenorTurma(Turma[] turmas) {
+        Turma menor = turmas[0];
+        for (Turma t : turmas) {
+            if (t.getQuantAlunos() < menor.getQuantAlunos()) {
+                menor = t;
+            }
+        }
+        return menor;
+    }
 }
